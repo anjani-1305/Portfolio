042c23487eb3af3d9b3d37760d07201929e4d31e
Anjani <kanjani142@gmail.com>
2023-10-19 13:06:17 +0530

    Final Working Portfolio Website

--- frontend/src/components/Newsletter.js
+++ frontend/src/components/Newsletter.js
@@ -1,48 +1,46 @@
-import { Alert, Col , Row } from "react-bootstrap"
-import { useState , useEffect } from "react"
-export const Newsletter = ({ subscribe , status , message }) => {
-    const [email , setEmail] = useState('');
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
 
-    useEffect( () => {
-        if(status==='success') clearFeilds();
-    }, [status])
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
 
-    const onValidated = (data) => {
-        console.log(data);
-    };
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        `email &&
-        email.indexOf("@") > -1` &&
-        onValidated({
-            EMAIL: email
-        })
-    }
-    const clearFeilds = () => {
-        setEmail('');
-    }
-    return (
-        <Col lg ={12}>
-            <div className="newsletter-bx">
-                <Row>
-                    <Col lg ={12} md ={6} xl ={5}>
-                        <h3>Subscribe To My Newsletter For My Latest Blogs and Projects</h3>
-                        {status === 'sending' && <Alert>Sending...</Alert>}
-                        {status === 'error' && <Alert variant="danger">{message}</Alert>}
-                        {status === 'success' && <Alert variant="success">{message}</Alert>}
-                    </Col>
-                    <Col md={6} xl={7}>
-                        <form onSubmit={handleSubmit}>
-                            <div className="new-email-bx">
-                                <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
-                                <button type="submit">Submit</button>
-                            </div>
-                            
-                        </form>
-                    </Col>
-                </Row>
-            </div>
-        </Col>
-    )
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
+              {status === 'sending' && <Alert>Sending...</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
+            </Col>
+            <Col md={6} xl={7}>
+              <form onSubmit={handleSubmit}>
+                <div className="new-email-bx">
+                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+  )
+}
